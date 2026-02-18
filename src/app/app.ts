4a9e3104949df1c697b8844690fb42a8e7e097b4
Uma-Raja <umakrishna750@gmail.com>
2026-02-18 23:55:03 +0530

    Initial commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,26 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('myApp');
+  task = signal('')
+  tasks = signal<string[]>([]);
+  addTask(){
+    if(this.task().trim()) {
+      this.tasks.update(list => [...list, this.task()]);
+      this.task.set('');
+    }
+  }
+  deleteTask(index:number){
+    this.tasks.update(list => list.filter((_, i) => i !== index));
+    
+  }
+  //protected readonly title = signal('myApp');
 }
